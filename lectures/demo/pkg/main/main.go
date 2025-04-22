f1ae79226683709cfc134027237414835afbe54d
michalkucharski <michal.kucharski33@gmail.com>
2025-04-22 19:11:54 -0400

    pkg

---
+++ lectures/demo/pkg/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"coursecontent/demo/pkg/display"
+	"coursecontent/demo/pkg/msg"
+)
+
+func main() {
+	msg.Hi()
+	display.Display("Hello from display")
+	msg.Exciting("an exciting message")
+
+}
