826af53648003cf4d8e554d7ede9f2e38b09fd2e
michalkucharski <michal.kucharski33@gmail.com>
2025-04-22 14:28:49 -0400

    reciever function

--- lectures/exercise/rcv-func/rcv-func.go
+++ lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,74 @@ package main
 
 import "fmt"
 
+type Player struct {
+	health, maxHealth uint
+	energy, maxEnergy uint
+	name              string
+}
+
+func (player *Player) addHealth(amount uint) {
+	player.health += uint(amount)
+
+	fmt.Println("Adding health point to player")
+	if player.health > player.maxHealth {
+		player.health = player.maxHealth
+	}
+	fmt.Println("Actual player statistics:", player)
+}
+
+func (player *Player) addEnergy(amount uint) {
+	player.energy += uint(amount)
+
+	fmt.Println("Adding energy point to player")
+	if player.energy > player.maxEnergy {
+		player.energy = player.maxEnergy
+	}
+	fmt.Println("Actual player statistics:", player)
+}
+
+func (player *Player) applyDamage(amount uint) {
+
+	fmt.Println("Apply damage to the player")
+	if player.health-amount > player.health {
+		player.health = 0
+		fmt.Println("Player is dead")
+	} else {
+		player.health -= amount
+	}
+
+	fmt.Println("Actual player statistics:", player)
+}
+
+func (player *Player) consumingEnergy(amount uint) {
+
+	fmt.Println("Consuming energy")
+
+	if player.energy-amount > player.energy {
+		fmt.Println("Player is exhausted")
+		player.energy = 0
+	} else {
+		player.energy -= amount
+
+	}
+
+	fmt.Println("Actual player statistics:", player)
+}
+
 func main() {
+
+	myPlayer := Player{
+		name:      "Gorag",
+		health:    200,
+		maxHealth: 200,
+		energy:    300,
+		maxEnergy: 400,
+	}
+
+	myPlayer.applyDamage(34)
+	myPlayer.consumingEnergy(100)
+	myPlayer.addEnergy(2)
+	myPlayer.addHealth(34)
+	myPlayer.consumingEnergy(600)
 
 }
