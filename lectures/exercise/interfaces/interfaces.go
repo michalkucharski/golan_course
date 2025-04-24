2b691b8ccf7e391e85690726819ee8eb099fa355
michalkucharski <michal.kucharski33@gmail.com>
2025-04-24 19:11:34 -0400

    interface exercise

--- lectures/exercise/interfaces/interfaces.go
+++ lectures/exercise/interfaces/interfaces.go
@@ -21,6 +21,64 @@ package main
 
 import "fmt"
 
+const (
+	Small = iota
+	Standard
+	Large
+)
+
+type Lift int
+
+type Liftpicker interface {
+	Picklift() Lift
+}
+
+type Motorcycle string
+type Car string
+type Truck string
+
+func (m Motorcycle) String() string {
+	return fmt.Sprintf("Motorcycle: %v", string(m))
+}
+
+func (t Truck) String() string {
+	return fmt.Sprintf("Truck: %v", string(t))
+}
+
+func (c Car) String() string {
+	return fmt.Sprintf("Car: %v", string(c))
+}
+
+func (m Motorcycle) Picklift() Lift {
+	return Small
+}
+
+func (c Car) Picklift() Lift {
+	return Standard
+}
+
+func (t Truck) Picklift() Lift {
+	return Large
+}
+
+func sendToLift(p Liftpicker) {
+	switch p.Picklift() {
+	case Small:
+		fmt.Printf(" send %v to small lift\n", p)
+	case Standard:
+		fmt.Printf(" send %v to standard lift\n", p)
+	case Large:
+		fmt.Printf(" send %v to large lift\n", p)
+	}
+}
+
 func main() {
+	car := Car("sedan")
+	truck := Truck("minery_truck")
+	motorcycle := Motorcycle("ktm")
+
+	sendToLift(car)
+	sendToLift(truck)
+	sendToLift(motorcycle)
 
 }
