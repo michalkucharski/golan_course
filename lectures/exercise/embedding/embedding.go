41b0836b9c463e529cf6ea8634e4895586d5644e
michalkucharski <michal.kucharski33@gmail.com>
2025-04-25 09:51:51 -0400

    embedding exercise

--- lectures/exercise/embedding/embedding.go
+++ lectures/exercise/embedding/embedding.go
@@ -12,9 +12,7 @@
 
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Bytes int
 type Celcius float32
@@ -31,8 +29,54 @@ type MemoryUsage struct {
 	amount []Bytes
 }
 
+func (c *CpuTemp) AverageCpu() int {
+	sumParam := 0
+	for i := 0; i < len(c.temp); i++ {
+		sumParam += int(c.temp[i])
+	}
+
+	return sumParam / len(c.temp)
+}
+
+func (m *MemoryUsage) AverageMem() int {
+	sumParam := 0
+	for i := 0; i < len(m.amount); i++ {
+		sumParam += int(m.amount[i])
+	}
+
+	return sumParam / len(m.amount)
+}
+
+func (b *BandwidthUsage) AverageBand() int {
+	sumParam := 0
+	for i := 0; i < len(b.amount); i++ {
+		sumParam += int(b.amount[i])
+	}
+
+	return sumParam / len(b.amount)
+}
+
+type Dashboard struct {
+	CpuTemp
+	MemoryUsage
+	BandwidthUsage
+}
+
+func (d Dashboard) String() string {
+	return fmt.Sprintf("Dashboard: CPU temp: %v, Memory Usage: %v, Bandwidth Usage: %v", d.temp, d.MemoryUsage.amount, d.BandwidthUsage.amount)
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
+	dash := Dashboard{
+		CpuTemp:        temp,
+		MemoryUsage:    memory,
+		BandwidthUsage: bandwidth,
+	}
+
+	fmt.Printf("Average usage: CPU %v, Memory %v, Bandwidth %v", dash.AverageCpu(), dash.AverageMem(), dash.AverageBand())
+
 }
