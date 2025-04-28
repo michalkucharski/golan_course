c6e702e2c85de49f5814fb047653eb6d6cc50cfa
michalkucharski <michal.kucharski33@gmail.com>
2025-04-28 18:33:51 -0400

    fanin, cancellation and generator demo

--- lectures/demo/generator/generator.go
+++ lectures/demo/generator/generator.go
@@ -1,4 +1,62 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateRandInt(min, max int) <-chan int {
+	out := make(chan int, 3)
+
+	go func() {
+		for {
+			out <- rand.Intn(max-min+1) + min
+		}
+	}()
+
+	return out
+}
+
+func generateRandIntn(count, min, max int) <-chan int {
+	out := make(chan int, 1)
+
+	go func() {
+		for i := 0; i < count; i++ {
+			out <- rand.Intn(max-min+1) + min
+		}
+		close(out)
+	}()
+	return out
+}
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	randInt := generateRandInt(1, 100)
+
+	fmt.Println("generateRandInt infinite")
+
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+	fmt.Println(<-randInt)
+
+	fmt.Println("generateRandInt using finite count")
+
+	randIntnRange := generateRandIntn(2, 1, 10)
+
+	for i := range randIntnRange {
+		fmt.Println(i)
+	}
+
+	fmt.Println("generateRandInt using infinite loop")
+	randIntn := generateRandIntn(3, 1, 10)
+
+	for {
+		n, open := <-randIntn
+		if !open {
+			break
+		}
+		fmt.Println(n)
+	}
 }
--- lectures/go.mod
+++ lectures/go.mod
@@ -3,6 +3,6 @@ module coursecontent
 go 1.21
 
 require (
-	github.com/chai2010/webp v1.4.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
+	github.com/chai2010/webp v1.4.0
+	github.com/google/uuid v1.6.0
 )
